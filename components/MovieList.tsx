342456a7ab1dc27d3541b5c8c0a7870ecdd302b2
ritchb <ritchb001@hotmail.com>
2023-05-08 21:16:56 -0400

    movie review app demo final

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -2,6 +2,8 @@ import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
 import * as web3 from '@solana/web3.js'
+import { MovieCoordinator } from "./coordinators/MovieCoordinator"
+import { Button, Center, HStack, Input, Spacer } from '@chakra-ui/react'
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
@@ -9,6 +11,7 @@ export const MovieList: FC = () => {
     const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
     const [page, setPage] = useState(1)
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)).then(async (accounts) => {
@@ -24,12 +27,25 @@ export const MovieList: FC = () => {
         MovieCoordinator.fetchPage(
             connection,
             page,
-            10
+            10,
+            search,
+            search !== ''
         ).then(setMovies)
     }, [page, search])
 
     return (
         <div>
+            <Center>
+                <Input
+                    id='search'
+                    color='gray.400'
+                    onChange={event => setSearch(event.currentTarget.value)}
+                    placeholder='Search'
+                    w='97%'
+                    mt={2}
+                    mb={2}
+                />
+            </Center>
             {
                 movies.map((movie, i) => <Card key={i} movie={movie} />)
             }
